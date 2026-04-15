d8897c3e0606ace479d82a2ca19f094f5e609523
RishitaSharma06 <rishita133pat@gmail.com>
2026-04-16 00:13:19 +0530

    Update UserModel.js

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -11,15 +11,5 @@ const userSchema = new mongoose.Schema({
   likedMovies: Array,
 });
 
-const User = mongoose.model("User", userSchema);
 
-try{
-  await User.deleteMany({});
-  await User.insertMany(DataTransfer,{ ordered: false });
-  console.log("Sample users inserted successfully.");
-} catch(error){
-  console.log("Error inserting sample users:", error.message);
-}
-    
-
-module.exports = User;
+module.exports=mongoose.model("User", userSchema);
