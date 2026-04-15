ed9c357e96e5cb717a23508936fc0b7f203bbe04
RishitaSharma06 <rishita133pat@gmail.com>
2026-04-15 15:38:22 +0530

    Add files via upload

---
+++ netflix-ui/src/components/Developers.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import {Link} from 'react-router-dom'
+import './Deveeloper/style.css'
+const Developers = () => {
+    return (
+      <>
+     
+      <section class="main">
+       <div className="mainB">
+        </div>
+      </section>
+      
+      </>
+      );
+}
+
+export default Developers
