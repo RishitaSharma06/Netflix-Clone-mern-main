bebdcc9be178d911ded43cc196c62d90125604fc
RishitaSharma06 <rishita133pat@gmail.com>
2026-04-15 16:59:24 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/UserRoutes.js");
+const db = require('./db.js');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+db_Url = process.env.ATLASDB_URL;
+
+db.connect(db_Url);
+
+
+app.get('/', (req, res) => {
+  res.send('Server is running successfully 🚀');
+});
+
+app.use("/api/user", userRoutes);
+
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
