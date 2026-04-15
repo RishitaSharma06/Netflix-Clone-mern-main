ee5ad23831abe5791e1dd5e0853cdcad9b2b5173
RishitaSharma06 <rishita133pat@gmail.com>
2026-04-16 01:15:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,13 +21,7 @@ app.get('/', (req, res) => {
 
 app.use("/api/user", userRoutes);
 
-const path = require('path');
 
-app.use(express.static(path.join(__dirname, "../netflix-ui/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../netflix-ui/build", "index.html"));
-});
 
 app.listen(5000, () => {
   console.log("server started on port 5000");
