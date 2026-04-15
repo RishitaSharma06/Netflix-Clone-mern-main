3e98ea63168195bec99ce0cbec6aa054adea3b4d
RishitaSharma06 <rishita133pat@gmail.com>
2026-04-16 01:09:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+const dns=require('node:dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
-dotenv.config();
 const userRoutes = require("./routes/UserRoutes.js");
 const db = require('./db.js');
 
@@ -10,13 +12,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-db.Database();
+const dbUrl=process.env.ATLASDB_URL;
+db.connect(dbUrl);
 
 app.get('/', (req, res) => {
   res.send('Server is running successfully 🚀');
 });
 
 app.use("/api/user", userRoutes);
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, "../netflix-ui/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../netflix-ui/build", "index.html"));
+});
 
 app.listen(5000, () => {
   console.log("server started on port 5000");
